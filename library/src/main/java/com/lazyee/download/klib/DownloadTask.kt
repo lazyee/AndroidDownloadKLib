6dae966bed80617dd0060edac2d687eaa45f683e
leeorz <378229364@qq.com>
2024-04-25 11:35:03 +0800

    优化下载代码

--- library/src/main/java/com/lazyee/download/klib/DownloadTask.kt
+++ library/src/main/java/com/lazyee/download/klib/DownloadTask.kt
@@ -173,7 +173,7 @@ class DownloadTask:BaseTask{
                     val buffer = ByteArray(bufferSize){-1}
                     val bufferedOutputStream = BufferedOutputStream(FileOutputStream(tempDownloadFilePath,true))
                     val bufferedInputStream = BufferedInputStream(httpUrlConnection.inputStream)
-                    val costTime = measureTimeMillis {
+//                    val costTime = measureTimeMillis {
                     var readSize = 0
                     while (bufferedInputStream.read(buffer).also { readSize = it } != -1 && !isCancelTask) {
                         if(isByteArrayEmpty(buffer)) throw DownloadFileReadEmptyValueException(downloadUrl,"读取在线资源错误")
@@ -184,8 +184,8 @@ class DownloadTask:BaseTask{
 
                         callbackDownloading()
                     }
-                    }
-                    LogUtils.e(TAG,"下载文件消耗的时间:$costTime")
+//                    }
+//                    LogUtils.e(TAG,"下载文件消耗的时间:$costTime")
 
                     if (alreadyDownloadSize == downloadFileProperty.contentLength || downloadFileProperty.contentLength <= 0) {//文件已经完整下载
                         File(tempDownloadFilePath).renameTo(File(downloadFilePath))
